f32ff9c725ef8c7c22f9ba51239bd87501bdef95
AliaaAhmed1507 <alyaa.ahmed.1507@gmail.com>
2022-12-13 01:05:27 +0200

    EX1

---
+++ C Language/C Basics Assignment/C Basics Assignment.c
@@ -0,0 +1,18 @@
+/*
+ ============================================================================
+ Name        : C.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void main() {
+	// Home_work-1
+	// EX1
+	printf("C Programming");
+}
