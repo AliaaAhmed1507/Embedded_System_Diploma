54de6695c6592c8f0d002c2666f3a8aaf8fbe7fb
AliaaAhmed1507 <alyaa.ahmed.1507@gmail.com>
2022-12-13 01:34:55 +0200

    EX2

---
+++ C Language/C Basics Assignment/Assignment.c
@@ -0,0 +1,19 @@
+/*
+ ============================================================================
+ Name        : Assignment.c
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	int n;
+	printf("Enter a integer: ");
+	fflush(stdin); fflush(stdout);
+	scanf("%d",&n);
+	fflush(stdin); fflush(stdout);
+	printf("You entered: %d",n);
+	fflush(stdin); fflush(stdout);
+	return 0;
+}
