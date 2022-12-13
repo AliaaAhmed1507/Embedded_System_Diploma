021f4dfc2003f2f3ba1a0a95e1d6411d300f1131
AliaaAhmed1507 <alyaa.ahmed.1507@gmail.com>
2022-12-13 07:23:54 +0200

    HW2 EX8

---
+++ C Language/C Basics Assignment/Assignment HW2 EX8.c
@@ -0,0 +1,45 @@
+/*
+ ============================================================================
+ Name        : Assignment.c
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	float n1 , n2 , result=0;
+	char c;
+
+	printf("Enter operator either + or - or * or divide : ");
+	fflush(stdin); fflush(stdout);
+	scanf("%c",&c);
+	fflush(stdin); fflush(stdout);
+
+	printf("Enter two operands : ");
+	fflush(stdin); fflush(stdout);
+	scanf("%f %f",&n1,&n2);
+
+	switch(c){
+	case '+' :{
+		result = n1 + n2;
+		}
+		break;
+	case '-':{
+		result = n1 - n2;
+		}
+		break;
+	case '*':{
+			result = n1 * n2;
+		}
+		break;
+	case '/':{
+			result = n1 / n2;
+		}
+		break;
+	}
+
+	printf("%f %c %f = %f",n1 ,c ,n1 ,result);
+	fflush(stdin); fflush(stdout);
+	return 0;
+}
