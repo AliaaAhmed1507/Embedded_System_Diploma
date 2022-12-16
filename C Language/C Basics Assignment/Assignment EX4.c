29ec00a240b4eef9d4ce2bd7d2cd09f0f4b75684
Alyaa Massoud <118786339+AliaaAhmed1507@users.noreply.github.com>
2022-12-16 05:57:24 +0200

    Update Assignment EX4.c

--- C Language/C Basics Assignment/Assignment EX4.c
+++ C Language/C Basics Assignment/Assignment EX4.c
@@ -8,13 +8,13 @@
 #include <stdlib.h>
 
 int main() {
-	float n1, n2 ,sum;
+	float n1, n2 ,P;
 	printf("Enter two integers: ");
 	fflush(stdin); fflush(stdout);
 	scanf("%f %f",&n1,&n2);
-	sum =n1 * n2;
+	P =n1 * n2;
 	fflush(stdin); fflush(stdout);
-	printf("Product: %f",sum);
+	printf("Product: %f",p);
 	fflush(stdin); fflush(stdout);
 	return 0;
 }
